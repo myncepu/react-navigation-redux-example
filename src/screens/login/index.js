06d9e7db572bd7ff160600310852abc439bae392
Yan Pengqiang <yan2010@live.com>
2018-05-30 18:34:26 +0800

    commit

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { StyleSheet, Text, View } from 'react-native'
-import Button from '../components/Button'
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'yellowgreen',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-})
+import {
+  Container,
+  Button,
+  Content,
+  Form,
+  Item,
+  Input,
+  Text,
+} from 'native-base'
 
 class LoginScreen extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      emailError: false,
+      password: '',
+      passwordError: false,
+    }
+  }
+
   static propTypes = {
     navigation: PropTypes.object.isRequired,
   }
@@ -29,20 +33,64 @@ class LoginScreen extends React.Component {
     }
   }
 
+  handleInputChange = (field, value) => {
+    const newState = {
+      ...this.state,
+      [field]: value,
+    }
+    this.setState(newState)
+  }
+
+  handleSubmit = () => {
+    // TODO: 不要只是切换login状态
+    // 下一步验证密码
+    this.props.navigation.dispatch({ type: 'Login' })
+
+    const { email, password } = this.state
+    if (email.length === 0) {
+      return this.setState({ emailError: true })
+    }
+    this.setState({ emailError: false })
+
+    if (password.length === 0) {
+      return this.setState({ passwordError: true })
+    }
+    this.setState({ passwordError: false })
+
+    return this.props.screenProps.changeLoginState(true)
+  }
+
   render() {
+    const { emailError, passwordError } = this.state
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-        Screen A
-        </Text>
-        <Text style={styles.instructions}>
-        This is great
-        </Text>
-        <Button
-          onPress={() => this.props.navigation.dispatch({ type: 'Login' })}
-          title="Log in"
-        />
-      </View>
+      <Container>
+        <Content>
+          <Form>
+            <Item error={emailError}>
+              <Input
+                placeholder="Email"
+                onChangeText={value => this.handleInputChange('email', value)}
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+              />
+            </Item>
+            <Item error={passwordError}>
+              <Input
+                placeholder="Password"
+                onChangeText={value => this.handleInputChange('password', value)}
+                autoCapitalize="none"
+                autoCorrect={false}
+                secureTextEntry
+              />
+            </Item>
+          </Form>
+          <Button full onPress={this.handleSubmit}>
+            <Text>Sign In</Text>
+          </Button>
+        </Content>
+      </Container>
     )
   }
 }
